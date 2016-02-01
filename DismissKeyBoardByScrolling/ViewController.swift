ab74f206a81d849d9b5c9ee5a579af6013099232
John Regner <regnerjr@gmail.com>
2016-01-31 19:10:19 -0800

    Finished up

--- DismissKeyBoardByScrolling/ViewController.swift
+++ DismissKeyBoardByScrolling/ViewController.swift
@@ -1,23 +1,50 @@
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UITableViewController {
 
-	@IBOutlet weak var tableView: UITableView!
+	lazy var data = Array(0..<50)
 
-	lazy var data = Array(0..<200)
-
-	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return data.count
 	}
 
-	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCellWithIdentifier(String(CustomCell), forIndexPath: indexPath) as! CustomCell
 		cell.label.text = String(data[indexPath.row])
 		return cell
+	}
+
+	override func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+		// Check if we have a first responder
+		guard let firstResponder = UIResponder.findFirstResponder() as? UIView else { return }
+		// 1
+		let offset = targetContentOffset.memory.y // Get the final Y offset
+		// 2
+		let rectInScrollView = scrollView.convertRect(firstResponder.bounds, fromView: firstResponder)
+		//3
+		let scrollViewFutureBounds = CGRect(origin: CGPoint(x: scrollView.contentOffset.x, y: offset ), size: scrollView.bounds.size)
+		// 4
+		if !scrollViewFutureBounds.contains(rectInScrollView) {
+			firstResponder.endEditing(true)
+		}
 	}
 }
 
 class CustomCell: UITableViewCell {
 	@IBOutlet var label: UILabel!
 	@IBOutlet var textField: UITextField!
+}
+
+extension UIResponder {
+	private static var responder: UIResponder? = nil
+
+	class func findFirstResponder() -> UIResponder? {
+		responder = nil
+		UIApplication.sharedApplication().sendAction("getFirstResponder", to: nil, from: nil, forEvent: nil)
+		return responder
+	}
+
+	func getFirstResponder() {
+		UIResponder.responder = self
+	}
 }
